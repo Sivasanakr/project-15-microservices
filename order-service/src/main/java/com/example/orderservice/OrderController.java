baa53ad81e606adc5b56380d5d7f77ad88f0ad05
kasulasivasankar <kasulasivasankar79@gmail.com>
2026-09-18 11:05:26 +0530

    intial commit - two microservices

---
+++ order-service/src/main/java/com/example/orderservice/OrderController.java
@@ -0,0 +1,16 @@
+package com.example.orderservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+    @GetMapping("/orders")
+    public String getOrders() {
+
+        return "Order Service is running successfully!";
+
+    }
+
+}
---
+++ user-service/src/main/java/com/example/userservice/UserController.java
@@ -0,0 +1,16 @@
+package com.example.userservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/users")
+    public String getUsers() {
+
+        return "User Service is running successfully!";
+
+    }
+
+}
